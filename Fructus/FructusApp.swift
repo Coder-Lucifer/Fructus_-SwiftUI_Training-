6a35a64d4c42ddb0e1830e8bac8e73d7e7d84fcd
Aviral Kandpal <aviral@debutinfotech.com>
2021-04-20 09:03:49 +0530

    Initial Commit

---
+++ Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by Aviral on 20/04/21.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
